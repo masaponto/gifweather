edb218906b7219f23fbd32ad7fe685ddec281ef2
masaponto <masaponto@gmail.com>
2017-03-19 18:47:12 +0900

    [init] use tablayout and view pages

---
+++ app/src/main/java/com/example/masato/weatherforecast/WeatherForecastActivity.java
@@ -0,0 +1,62 @@
+package com.example.masato.weatherforecast;
+
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.masato.weatherforecast.databinding.ActivityWeatherForecastBinding;
+
+public class WeatherForecastActivity extends AppCompatActivity
+        implements WeekForecastFragment.OnFragmentInteractionListener,
+        DaysForecastFragment.OnFragmentInteractionListener {
+
+    private ActivityWeatherForecastBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_forecast);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_weather_forecast);
+
+        TabLayout tabLayout = binding.tabs;
+        ViewPager pager = binding.pager;
+        final String[] pageTitle = {"3 days", "1 week"};
+
+        FragmentPagerAdapter adapter  = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return DaysForecastFragment.newInstance(position+1);
+                    case 1:
+                        return WeekForecastFragment.newInstance(position+1);
+                }
+                return null;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return pageTitle[position];
+            }
+
+            @Override
+            public int getCount() {
+                return pageTitle.length;
+            }
+        };
+
+        pager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/com/example/masato/weatherforecast/WeekForecastFragment.java
@@ -0,0 +1,111 @@
+package com.example.masato.weatherforecast;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.masato.weatherforecast.databinding.FragmentWeekForecastBinding;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link WeekForecastFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link WeekForecastFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WeekForecastFragment extends Fragment {
+
+    // TODO: Rename and change types of parameters
+    private int page;
+
+    private OnFragmentInteractionListener mListener;
+    private FragmentWeekForecastBinding binding;
+
+    public WeekForecastFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param page Parameter 1.
+     * @return A new instance of fragment WeekForecastFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WeekForecastFragment newInstance(int page) {
+        WeekForecastFragment fragment = new WeekForecastFragment();
+        Bundle args = new Bundle();
+        args.putInt("page", page);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            page = getArguments().getInt("page");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_week_forecast, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentWeekForecastBinding.bind(view);
+        binding.hoge.setText("week forecast");
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
